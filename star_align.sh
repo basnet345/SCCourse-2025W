cb9d75621c45655a716afb86ac2856512f3630ec
basnet345 <sarsotib@gmail.com>
2026-01-22 01:00:51 +0100

    Add comments to scripts
    Add comments to scripts

--- star_align.sh
+++ star_align.sh
@@ -1,22 +1,53 @@
 #!/bin/bash
 
 #SBATCH --job-name=STAR
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=20G
-#SBATCH --time=12:00:00
-#SBATCH --output=/lisc/data/scratch/course/2025w300106/basnet/logs/STAR.%j.log
-#SBATCH --error=/lisc/data/scratch/course/2025w300106/basnet/logs/STAR.%j.err
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=a12448596@unet.univie.ac.at
+# name of the job
 
-###ENVIRONMENT
+#SBATCH --nodes=1
+# request one node
+
+#SBATCH --cpus-per-task=16
+# number of CPUs to use
+
+#SBATCH --mem=20G
+# memory requested for the job
+
+#SBATCH --time=12:00:00
+# maximum run time
+
+#SBATCH --output=/lisc/data/scratch/course/2025w300106/basnet/logs/STAR.%j.log
+# file to write standard output
+# %j is the job ID
+
+#SBATCH --error=/lisc/data/scratch/course/2025w300106/basnet/logs/STAR.%j.err
+# file to write error messages
+
+#SBATCH --mail-type=END,FAIL
+# send email when job ends or fails
+
+#SBATCH --mail-user=a12448596@unet.univie.ac.at
+# email address for notifications
+
+
+### ENVIRONMENT
+# load STAR module
 module load STAR
+
+# show loaded modules in log
 module list
 
-###EXECUTION
-STAR --genomeDir /lisc/data/scratch/course/2025w300106/basnet/Nem_STAR --runThreadN 16 --readFilesIn /lisc/data/scratch/course/2025w300106/basnet/SRR33559393_1.fastq /lisc/data/scratch/course/2025w300106/basnet/SRR33559393_2.fastq --outFileNamePrefix /lisc/data/scratch/course/2025w300106/basnet/results/map/SRR33559393_Nem_ --outSAMstrandField intronMotif --outSAMtype BAM SortedByCoordinate
 
-
-
-
+### EXECUTION
+# run STAR for paired-end read alignment
+# use pre-built genome index
+# use 16 threads
+# output sorted BAM file
+# include strand information for downstream tools
+STAR \
+  --genomeDir /lisc/data/scratch/course/2025w300106/basnet/Nem_STAR \
+  --runThreadN 16 \
+  --readFilesIn /lisc/data/scratch/course/2025w300106/basnet/SRR33559393_1.fastq \
+                /lisc/data/scratch/course/2025w300106/basnet/SRR33559393_2.fastq \
+  --outFileNamePrefix /lisc/data/scratch/course/2025w300106/basnet/results/map/SRR33559393_Nem_ \
+  --outSAMstrandField intronMotif \
+  --outSAMtype BAM SortedByCoordinate
